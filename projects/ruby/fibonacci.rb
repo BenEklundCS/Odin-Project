632023f94ae22bfbf86c6e5c3ccc071247372c5c
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2023-09-25 22:29:20 +0000

    no need for duplicate recipe project

--- projects/ruby/fibonacci.rb
+++ projects/ruby/fibonacci.rb
@@ -1,11 +1,11 @@
 # Recursive fibonacci solution
 def fib(n)
-    (n <= 1 ) ? n : fib(n-1) + fib(n-2) # if (n <= 1) is true, return n. Otherwise, return the result of fib(n-1) + fib(n-2) (recursion!)
+    # if (n <= 1) is true, return n. Otherwise, return the result of fib(n-1) + fib(n-2) (recursion!)
+    (n <= 1 ) ? n : fib(n-1) + fib(n-2)
 end
 
-puts fib(1)
-puts fib(2)
-puts fib(3)
-puts fib(5)
-
-puts fib(10)
+puts fib(1) # --> 1
+puts fib(2) # --> 1
+puts fib(3) # --> 2
+puts fib(5) # --> 5
+puts fib(10) # --> 55
