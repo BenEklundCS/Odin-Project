b96fbe97249e75032ee3830ee5ca478f851783b9
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2023-10-08 15:55:17 +0000

    JS Fundamentals Part 3

---
+++ basics/basic_webdev/basic_javascript/functions.js
@@ -0,0 +1,21 @@
+function add7(num) {
+    return num + 7
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function last_letter(string) {
+    return string.charAt(string.length-1)
+}
+
+console.log(add7(10))
+console.log(multiply(17, 4))
+console.log(capitalize("ben"))
+console.log(last_letter("Gamer time"))
+
